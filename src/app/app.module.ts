8b03f59450710003886a8181176c7cd92bed8037
Samet Kostak <sametkostak@gmail.com>
2022-03-16 15:17:35 +0300

    logins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,145 +1,149 @@
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LocationStrategy, HashLocationStrategy, PathLocationStrategy} from '@angular/common';
-import {AppRoutingModule} from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationStrategy, HashLocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import {
+    FacebookLoginProvider,
+    SocialLoginModule,
+    SocialAuthServiceConfig,
+} from 'angularx-social-login';
+import { AccordionModule } from 'primeng/accordion';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { ChartModule } from 'primeng/chart';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import { ChipsModule } from 'primeng/chips';
+import { CodeHighlighterModule } from 'primeng/codehighlighter';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DataViewModule } from 'primeng/dataview';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { FieldsetModule } from 'primeng/fieldset';
+import { FileUploadModule } from 'primeng/fileupload';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { GalleriaModule } from 'primeng/galleria';
+import { ImageModule } from 'primeng/image';
+import { InplaceModule } from 'primeng/inplace';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { KnobModule } from 'primeng/knob';
+import { LightboxModule } from 'primeng/lightbox';
+import { ListboxModule } from 'primeng/listbox';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { OrderListModule } from 'primeng/orderlist';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { PaginatorModule } from 'primeng/paginator';
+import { PanelModule } from 'primeng/panel';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { PasswordModule } from 'primeng/password';
+import { PickListModule } from 'primeng/picklist';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { SkeletonModule } from 'primeng/skeleton';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { SliderModule } from 'primeng/slider';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SplitterModule } from 'primeng/splitter';
+import { StepsModule } from 'primeng/steps';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { TagModule } from 'primeng/tag';
+import { TerminalModule } from 'primeng/terminal';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { TimelineModule } from 'primeng/timeline';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
+import { TreeModule } from 'primeng/tree';
+import { TreeTableModule } from 'primeng/treetable';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
 
-import {AccordionModule} from 'primeng/accordion';
-import {AutoCompleteModule} from 'primeng/autocomplete';
-import {AvatarModule} from 'primeng/avatar';
-import {AvatarGroupModule} from 'primeng/avatargroup';
-import {BadgeModule} from 'primeng/badge';
-import {BreadcrumbModule} from 'primeng/breadcrumb';
-import {ButtonModule} from 'primeng/button';
-import {CalendarModule} from 'primeng/calendar';
-import {CardModule} from 'primeng/card';
-import {CarouselModule} from 'primeng/carousel';
-import {CascadeSelectModule} from 'primeng/cascadeselect';
-import {ChartModule} from 'primeng/chart';
-import {CheckboxModule} from 'primeng/checkbox';
-import {ChipModule} from 'primeng/chip';
-import {ChipsModule} from 'primeng/chips';
-import {CodeHighlighterModule} from 'primeng/codehighlighter';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
-import {ConfirmPopupModule} from 'primeng/confirmpopup';
-import {ColorPickerModule} from 'primeng/colorpicker';
-import {ContextMenuModule} from 'primeng/contextmenu';
-import {DataViewModule} from 'primeng/dataview';
-import {DialogModule} from 'primeng/dialog';
-import {DividerModule} from 'primeng/divider';
-import {DropdownModule} from 'primeng/dropdown';
-import {FieldsetModule} from 'primeng/fieldset';
-import {FileUploadModule} from 'primeng/fileupload';
-import {FullCalendarModule} from '@fullcalendar/angular';
-import {GalleriaModule} from 'primeng/galleria';
-import {ImageModule} from 'primeng/image';
-import {InplaceModule} from 'primeng/inplace';
-import {InputNumberModule} from 'primeng/inputnumber';
-import {InputMaskModule} from 'primeng/inputmask';
-import {InputSwitchModule} from 'primeng/inputswitch';
-import {InputTextModule} from 'primeng/inputtext';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import {KnobModule} from 'primeng/knob';
-import {LightboxModule} from 'primeng/lightbox';
-import {ListboxModule} from 'primeng/listbox';
-import {MegaMenuModule} from 'primeng/megamenu';
-import {MenuModule} from 'primeng/menu';
-import {MenubarModule} from 'primeng/menubar';
-import {MessagesModule} from 'primeng/messages';
-import {MessageModule} from 'primeng/message';
-import {MultiSelectModule} from 'primeng/multiselect';
-import {OrderListModule} from 'primeng/orderlist';
-import {OrganizationChartModule} from 'primeng/organizationchart';
-import {OverlayPanelModule} from 'primeng/overlaypanel';
-import {PaginatorModule} from 'primeng/paginator';
-import {PanelModule} from 'primeng/panel';
-import {PanelMenuModule} from 'primeng/panelmenu';
-import {PasswordModule} from 'primeng/password';
-import {PickListModule} from 'primeng/picklist';
-import {ProgressBarModule} from 'primeng/progressbar';
-import {RadioButtonModule} from 'primeng/radiobutton';
-import {RatingModule} from 'primeng/rating';
-import {RippleModule} from 'primeng/ripple';
-import {ScrollPanelModule} from 'primeng/scrollpanel';
-import {ScrollTopModule} from 'primeng/scrolltop';
-import {SelectButtonModule} from 'primeng/selectbutton';
-import {SidebarModule} from 'primeng/sidebar';
-import {SkeletonModule} from 'primeng/skeleton';
-import {SlideMenuModule} from 'primeng/slidemenu';
-import {SliderModule} from 'primeng/slider';
-import {SplitButtonModule} from 'primeng/splitbutton';
-import {SplitterModule} from 'primeng/splitter';
-import {StepsModule} from 'primeng/steps';
-import {TabMenuModule} from 'primeng/tabmenu';
-import {TableModule} from 'primeng/table';
-import {TabViewModule} from 'primeng/tabview';
-import {TagModule} from 'primeng/tag';
-import {TerminalModule} from 'primeng/terminal';
-import {TieredMenuModule} from 'primeng/tieredmenu';
-import {TimelineModule} from 'primeng/timeline';
-import {ToastModule} from 'primeng/toast';
-import {ToggleButtonModule} from 'primeng/togglebutton';
-import {ToolbarModule} from 'primeng/toolbar';
-import {TooltipModule} from 'primeng/tooltip';
-import {TreeModule} from 'primeng/tree';
-import {TreeTableModule} from 'primeng/treetable';
-import {VirtualScrollerModule} from 'primeng/virtualscroller';
-
-import {AppComponent} from './app.component';
-import {AppCodeModule} from './app-components/app.code.component'
-import {AppMainComponent} from './app-components/app.main.component';
-import {AppCrudComponent} from './pages/app.crud.component';
-import {AppCalendarComponent} from './pages/app.calendar.component';
-import {AppTimelineDemoComponent} from './pages/app.timelinedemo.component';
-import {AppNotfoundComponent} from './pages/app.notfound.component';
-import {AppErrorComponent} from './pages/app.error.component';
-import {AppAccessdeniedComponent} from './pages/app.accessdenied.component';
-import {AppLoginComponent} from './pages/app.login.component';
-import {AppMenuComponent} from './app-components/app.menu.component';
-import {AppMenuitemComponent} from './app-components/app.menuitem.component';
-import {AppRightMenuComponent} from './app-components/app.right-menu.component';
-import {AppTopBarComponent} from './app-components/app.topbar.component';
-import {AppFooterComponent} from './app-components/app.footer.component';
-import {FormLayoutDemoComponent} from './demo/view/formlayoutdemo.component';
-import {FloatLabelDemoComponent} from './demo/view/floatlabeldemo.component';
-import {InvalidStateDemoComponent} from './demo/view/invalidstatedemo.component';
-import {InputDemoComponent} from './demo/view/inputdemo.component';
-import {ButtonDemoComponent} from './demo/view/buttondemo.component';
-import {TableDemoComponent} from './demo/view/tabledemo.component';
+import { AppComponent } from './app.component';
+import { AppCodeModule } from './app-components/app.code.component'
+import { AppMainComponent } from './app-components/app.main.component';
+import { AppCrudComponent } from './pages/app.crud.component';
+import { AppCalendarComponent } from './pages/app.calendar.component';
+import { AppTimelineDemoComponent } from './pages/app.timelinedemo.component';
+import { AppNotfoundComponent } from './pages/app.notfound.component';
+import { AppErrorComponent } from './pages/app.error.component';
+import { AppAccessdeniedComponent } from './pages/app.accessdenied.component';
+import { AppLoginComponent } from './pages/app.login.component';
+import { AppMenuComponent } from './app-components/app.menu.component';
+import { AppMenuitemComponent } from './app-components/app.menuitem.component';
+import { AppRightMenuComponent } from './app-components/app.right-menu.component';
+import { AppTopBarComponent } from './app-components/app.topbar.component';
+import { AppFooterComponent } from './app-components/app.footer.component';
+import { FormLayoutDemoComponent } from './demo/view/formlayoutdemo.component';
+import { FloatLabelDemoComponent } from './demo/view/floatlabeldemo.component';
+import { InvalidStateDemoComponent } from './demo/view/invalidstatedemo.component';
+import { InputDemoComponent } from './demo/view/inputdemo.component';
+import { ButtonDemoComponent } from './demo/view/buttondemo.component';
+import { TableDemoComponent } from './demo/view/tabledemo.component';
 import { CustomersComponent } from './tables/customer-tables/customers-list.component';
-import {ListDemoComponent} from './demo/view/listdemo.component';
-import {TreeDemoComponent} from './demo/view/treedemo.component';
-import {PanelsDemoComponent} from './demo/view/panelsdemo.component';
-import {OverlaysDemoComponent} from './demo/view/overlaysdemo.component';
-import {MediaDemoComponent} from './demo/view/mediademo.component';
-import {MenusDemoComponent} from './demo/view/menusdemo.component';
-import {MessagesDemoComponent} from './demo/view/messagesdemo.component';
-import {MiscDemoComponent} from './demo/view/miscdemo.component';
-import {EmptyDemoComponent} from './demo/view/emptydemo.component';
-import {ChartsDemoComponent} from './demo/view/chartsdemo.component';
-import {FileDemoComponent} from './demo/view/filedemo.component';
-import {DashboardDemoComponent} from './demo/view/dashboarddemo.component';
-import {DocumentationComponent} from './demo/view/documentation.component';
-import {DisplayComponent} from './utilities/display.component';
-import {ElevationComponent} from './utilities/elevation.component';
-import {FlexboxComponent} from './utilities/flexbox.component';
-import {GridComponent} from './utilities/grid.component';
-import {IconsComponent} from './utilities/icons.component';
-import {SpacingComponent} from './utilities/spacing.component';
-import {TypographyComponent} from './utilities/typography.component';
-import {TextComponent} from './utilities/text.component';
-import {WidgetsComponent} from './utilities/widgets.component';
+import { ListDemoComponent } from './demo/view/listdemo.component';
+import { TreeDemoComponent } from './demo/view/treedemo.component';
+import { PanelsDemoComponent } from './demo/view/panelsdemo.component';
+import { OverlaysDemoComponent } from './demo/view/overlaysdemo.component';
+import { MediaDemoComponent } from './demo/view/mediademo.component';
+import { MenusDemoComponent } from './demo/view/menusdemo.component';
+import { MessagesDemoComponent } from './demo/view/messagesdemo.component';
+import { MiscDemoComponent } from './demo/view/miscdemo.component';
+import { EmptyDemoComponent } from './demo/view/emptydemo.component';
+import { ChartsDemoComponent } from './demo/view/chartsdemo.component';
+import { FileDemoComponent } from './demo/view/filedemo.component';
+import { DashboardDemoComponent } from './demo/view/dashboarddemo.component';
+import { DocumentationComponent } from './demo/view/documentation.component';
+import { DisplayComponent } from './utilities/display.component';
+import { ElevationComponent } from './utilities/elevation.component';
+import { FlexboxComponent } from './utilities/flexbox.component';
+import { GridComponent } from './utilities/grid.component';
+import { IconsComponent } from './utilities/icons.component';
+import { SpacingComponent } from './utilities/spacing.component';
+import { TypographyComponent } from './utilities/typography.component';
+import { TextComponent } from './utilities/text.component';
+import { WidgetsComponent } from './utilities/widgets.component';
 
-import {CountryService} from './demo/service/countryservice';
-import {EventService} from './demo/service/eventservice';
-import {NodeService} from './demo/service/nodeservice';
-import {MenuService} from './app-components/app.menu.service';
-import {CustomerService} from './demo/service/customerservice';
-import {PhotoService} from './demo/service/photoservice';
-import {ProductService} from './demo/service/productservice';
-import {IconService} from './demo/service/iconservice';
+import { CountryService } from './demo/service/countryservice';
+import { EventService } from './demo/service/eventservice';
+import { NodeService } from './demo/service/nodeservice';
+import { MenuService } from './app-components/app.menu.service';
+import { CustomerService } from './demo/service/customerservice';
+import { PhotoService } from './demo/service/photoservice';
+import { ProductService } from './demo/service/productservice';
+import { IconService } from './demo/service/iconservice';
 
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -155,7 +159,7 @@ FullCalendarModule.registerPlugins([
     interactionPlugin
 ]);
 
-export function tokenGetter(){
+export function tokenGetter() {
     return localStorage.getItem(`jwt`);
 }
 
@@ -248,13 +252,15 @@ export function tokenGetter(){
         TreeTableModule,
         VirtualScrollerModule,
         JwtModule.forRoot({
-            config:{
-                tokenGetter:tokenGetter,
-                allowedDomains:[
+            config: {
+                tokenGetter: tokenGetter,
+                allowedDomains: [
                     'localhost:44350'
                 ],
             }
-        })
+        }),
+        ReactiveFormsModule,
+        SocialLoginModule,
     ],
     declarations: [
         AppComponent,
@@ -305,10 +311,22 @@ export function tokenGetter(){
         CustomerDetailsComponent
     ],
     providers: [
+        {
+            provide: 'SocialAuthServiceConfig',
+            useValue: {
+                autoLogin: false,
+                providers: [
+                    {
+                        id: FacebookLoginProvider.PROVIDER_ID,
+                        provider: new FacebookLoginProvider('678816283240557'),
+                    }
+                ],
+            } as SocialAuthServiceConfig,
+        },
         [AuthGuardService],
-        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        { provide: LocationStrategy, useClass: PathLocationStrategy },
         CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService, MenuService
+        PhotoService, ProductService, MenuService,
     ],
     bootstrap: [AppComponent]
 })
--- src/app/pages/app.login.component.ts
+++ src/app/pages/app.login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FacebookLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
 import { Message, MessageService } from 'primeng/api';
 import { UserModel } from '../models/user';
 
@@ -15,19 +16,39 @@ import { UserModel } from '../models/user';
 			margin-left: .25em;
 		}
     `],
-    styleUrls:['./app.login.component.scss'],
+  styleUrls: ['./app.login.component.scss'],
   providers: [],
   templateUrl: './app.login.component.html',
 })
-export class AppLoginComponent {
+export class AppLoginComponent implements OnInit {
 
-  constructor(private httpClient: HttpClient, private router: Router) {
-
-  }
 
   item: UserModel = { userName: null, password: null };
   loginUrl: string = 'https://localhost:44350/api/User/Login';
   invalidLogin: boolean;
+  socialUser!: SocialUser;
+  
+  constructor(private httpClient: HttpClient,
+    private router: Router,
+    private socialAuthService: SocialAuthService,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.invalidLogin = user == null;
+      if (!this.invalidLogin) {
+        const token = (<any>user).token;
+        localStorage.setItem('jwt', token);
+        this.router.navigate(['/']);
+      } else {
+        this.router.navigate(['/login']);
+      }
+    })
+  }
+
+
 
   login(data: UserModel) {
     this.httpClient.post(this.loginUrl, data, {
@@ -44,7 +65,9 @@ export class AppLoginComponent {
     });
   }
 
-  
+  loginWithFacebook() {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
 
 
 }
